76660a349bbdeefeeba99b7ede7316da139147d5
Bennett Dams <dams.bennett@gmail.com>
2022-07-25 11:42:11 +0200

    Functional updates for counter example
    Good practice to use functional updates, as per docs: https://reactjs.org/docs/hooks-reference.html#functional-updates

--- src/counter.tsx
+++ src/counter.tsx
@@ -6,8 +6,8 @@ export function Counter({ initialCount }: { initialCount: number }) {
   return (
     <>
       count: {count}
-      <Button label="+1" onClick={() => setCount(count + 1)} />
-      <Button label="-1" onClick={() => setCount(count - 1)} />
+      <Button label="+1" onClick={(prevCount) => setCount(prevCount + 1)} />
+      <Button label="-1" onClick={(prevCount) => setCount(prevCount - 1)} />
       <Button label="reset" onClick={() => setCount(0)} />
     </>
   )
